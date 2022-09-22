62b81a0298f6ec97866d90bec1ee5e657b1b26af
rohit raout <rohitr204btechcse2024@kccitm.edu.in>
2022-09-22 03:48:37 -0700

    .sdfhhisu

---
+++ m.py
@@ -0,0 +1,10 @@
+
+# Tuples in Python - Hacker Rank Solution
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    # Tuples in Python - Hacker Rank Solution START
+    t = tuple(integer_list)
+    print(hash(t));
+    # Tuples in Python - Hacker Rank Solution END
+3
